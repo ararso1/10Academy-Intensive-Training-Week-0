17a36ae2f5b638ff7fb7e74c1e596f854a4438a9
ararso1 <arasoalisho2@gmail.com>
2024-08-31 23:47:10 +0300

    python version issue fixed

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,34 +4,8 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import plotly.graph_objs as go
 from plotly import express as px
-from db.models import *
-from db.db_config import engine, Base, SessionLocal
 import pandas as pd
 from scripts import helper, topic_and_event_modeling
-# Create all tables
-Base.metadata.create_all(bind=engine)
-
-def load_data(data_df):
-    session = SessionLocal()
-
-    # Convert data to dictionaries for insertion
-    articles_data = data_df[['article_id', 'source_name', 'published_at', 'title', 'full_content']].to_dict(orient='records')
-    topics_data = [{'topic_keywords': ', '.join(topics[i]), 'article_id': row['article_id']} for i, row in data_df.iterrows()] # type: ignore
-    events_data = [{'event_description': row['event_description'], 'article_id': row['article_id']} for i, row in data_df.iterrows()]
-    features_data = [{'article_id': row['article_id'], 'tfidf_vector': str(tfidf_matrix[i].toarray()), 'topic': row['topic'], 'event_cluster': row['event_cluster']} for i, row in data_df.iterrows()] # type: ignore
-
-    try:
-        # Insert data into tables
-        session.bulk_insert_mappings(Article, articles_data)
-        session.bulk_insert_mappings(Topic, topics_data)
-        session.bulk_insert_mappings(Event, events_data)
-        session.bulk_insert_mappings(Feature, features_data)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        print(f"An error occurred: {e}")
-    finally:
-        session.close()
 
 # Function to get the top ten websites by news article count
 def get_top_articles(data_df):
---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+# tests/test_analysis.py
+import pytest
+import unittest
+
+# tests/test_sample.py
+
+import unittest
+
+class TestSample(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_subtraction(self):
+        self.assertEqual(2 - 1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
